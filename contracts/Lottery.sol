98cb2822b8ffa40ec2c748e77e120d50bcae2cc3
ankit <ankitjain19962013@gmail.com>
2021-10-31 04:28:16 +0530

    contract update

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,35 +1,37 @@
-pragma solidity ^0.4.17;
+pragma solidity >0.4.99 <0.6.0;
 
 contract Lottery {
     address public manager;
-    address[] public players;
+    address payable[] private players;
 
-    function Lottery() public {
+    constructor() public {
         manager = msg.sender;
     }
-
+    
     function enter() public payable {
-        require(msg.value > .01 ether);
-
+        require(msg.value > .01 ether, "Must have at least 0.01 ether");
+        
         players.push(msg.sender);
     }
-
-    function random() private view returns (uint) {
-        return uint(keccak256(block.difficulty, now, players));
-    }
-
+    
     function pickWinner() public restricted {
         uint index = random() % players.length;
-        players[index].transfer(this.balance);
-        players = new address[](0);
+        address payable winner = address(players[index]);
+        address contest = address(this);
+        winner.transfer(contest.balance);
+        players = new address payable[](0);
     }
-
-    modifier restricted() {
-        require(msg.sender == manager);
-        _;
-    }
-
-    function getPlayers() public view returns (address[]) {
+    
+    function getPlayers() public restricted view returns (address payable[] memory) {
         return players;
+    }
+    
+    function random() private view returns (uint) {
+        return uint(keccak256(abi.encodePacked(block.difficulty, now, players)));
+    }
+    
+    modifier restricted() {
+        require(manager == msg.sender, "Only Manager can pick the winner");
+        _;
     }
 }
